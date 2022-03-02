5912997cf783888eb473838985cbc2004c59dcd6
ErickTraore <traoreerick@gmail.com>
2022-03-02 09:24:07 +0100

    projet node

---
+++ frontend/.history/src/server_20220203182722.js
@@ -0,0 +1,23 @@
+const fileUpload = require("express-fileupload");
+
+ Body Parser configuration
+ server.use(bodyParser.urlencoded({ extended: true }));
+// server.use(express.static('public'));
+// server.use(bodyParser.json());
+// server.use(bodyParser.urlencoded({ extended: false }))
+
+
+
+// server.use(express.json());
+// server.use(express.urlencoded({ extended: true }));
+// server.use(fileUpload());
+
+var http = require('http');
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    var message = '<h1>test4 ikcad.net test works <h1>\n',
+        version = 'NodeJS ' + process.versions.node + '\n',
+        response = [message, version].join('\n');
+    res.end(response);
+});
+server.listen()
